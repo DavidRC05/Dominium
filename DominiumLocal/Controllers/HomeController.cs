2fb8aed6cd242d30ff657598f87de9709c0eec96
DavidRC05 <davidrocas04@outlook.com>
2023-09-27 02:31:09 -0600

    Creacion del Layout (nav) y pequeno Login

--- DominiumLocal/Controllers/HomeController.cs
+++ DominiumLocal/Controllers/HomeController.cs
@@ -13,16 +13,9 @@ namespace DominiumLocal.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
